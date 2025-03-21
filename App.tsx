1db2bbcfb08c4c618f2b98f38779f8eb3f9ec133
LipeArcanjo <lipearcanjoms@gmail.com>
2025-03-20 23:47:40 -0300

    feat: add page navigation in App.tsx file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BeneficiariosScreen from './src/screens/BeneficiariosScreen';
+import FuncionariosScreen from './src/screens/FuncionariosScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SplashScreen from './src/screens/SplashScreen';
+ 
+const Stack = createStackNavigator();
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<NavigationContainer>
+<Stack.Navigator initialRouteName="Splash">
+<Stack.Screen name="Splash" component={SplashScreen}/>
+<Stack.Screen name="Login" component={LoginScreen}/>
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Beneficiarios" component={BeneficiariosScreen} />
+<Stack.Screen name="Funcionarios" component={FuncionariosScreen} />
+</Stack.Navigator>
+</NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
